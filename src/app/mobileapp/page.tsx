7cab96fac841935b698c25f59d29b9d8fcc352bd
Jesse van Luxemburg <jessevanluxemburg@gmail.com>
2025-04-03 10:31:14 +0200

    icon change

--- src/app/mobileapp/page.tsx
+++ src/app/mobileapp/page.tsx
@@ -178,12 +178,9 @@ export default function MobileApp() {
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M12 4.5v15m7.5-7.5h-15"
-            />
+            <circle cx="12" cy="12" r="10" strokeWidth="2" />
+            <circle cx="12" cy="12" r="6" strokeWidth="2" />
+            <circle cx="12" cy="12" r="2" strokeWidth="2" />
           </svg>
         </button>
         <button
@@ -232,7 +229,7 @@ export default function MobileApp() {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
-              d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+              d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
             />
           </svg>
         </button>
